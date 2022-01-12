40f32977c7b94e06940b4c31a874f984bb495dfd
julienliguori <julien.liguori@viacesi.fr>
2022-01-12 11:46:45 +0100

    a lot of changes

---
+++ Assets/VR-Vs-KMS/Scripts/PlayerWeapon.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon;
+using Photon.Pun;
+
+public abstract class PlayerWeapon : MonoBehaviourPun
+{
+    public GameObject Bullet;
+    public GameObject ShootPoint;
+    private string WeaponName;
+    private int WeaponDamage;
+
+    public PlayerWeapon(string WeaponName, int WeaponDamage)
+    {
+        this.WeaponName = WeaponName;
+        this.WeaponDamage = WeaponDamage;
+    }
+
+    void Start()
+    {
+        WeaponStart();
+    }
+    
+    public void Shoot()
+    {
+        photonView.RPC("RpcShoot", RpcTarget.All);
+    }
+
+    void Update()
+    {
+        WeaponUpdate();
+    }
+
+    public abstract void WeaponStart();
+    public abstract void WeaponUpdate();
+}
---
+++ Assets/VR-Vs-KMS/Scripts/Weapons/PP1.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class PP1 : PlayerWeapon
+{
+    public PP1() : base("PP1",20) { }
+    public float DelayShoot;
+    private bool delay = false;
+
+    [PunRPC]
+    public void RpcShoot()
+    {
+        if(!delay)
+        {
+            delay = true;
+            StartCoroutine(ShootWithDelay());
+        }
+    }
+
+    IEnumerator ShootWithDelay()
+    {
+        GameObject bullet = Instantiate(Bullet,ShootPoint.transform.position,ShootPoint.transform.rotation);
+        Rigidbody br = bullet.GetComponent<Rigidbody>();
+        br.AddRelativeForce(Vector3.forward * 1000 * Time.deltaTime, ForceMode.Impulse);
+        yield return new WaitForSeconds(DelayShoot);
+        delay = false;
+        
+    }
+
+    // Start is called before the first frame update
+    public override void WeaponStart()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public override void WeaponUpdate()
+    {
+        
+    }
+}
