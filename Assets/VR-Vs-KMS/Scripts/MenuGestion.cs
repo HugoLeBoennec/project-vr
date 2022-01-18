f39925f9b79ee6311dfdd57e0f57ea09cd00a927
EtienneRog <etienne.roger@viacesi.fr>
2022-01-18 12:09:19 +0100

    "Ajout de la detction du type de joueur choisi a la selection du bouton"

--- Assets/VR-Vs-KMS/Scripts/MenuGestion.cs
+++ Assets/VR-Vs-KMS/Scripts/MenuGestion.cs
@@ -3,15 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
+public static class CurrentName{
+    public static string typePlayer;
+}
 public class MenuGestion : MonoBehaviour
 {
 
     public void PlayGameKMS()
     {
+        CurrentName.typePlayer = "KMS";
         SceneManager.LoadScene("GameScene");
     }
     public void PlayGameVirus()
     {
+        CurrentName.typePlayer = "Virus";
         SceneManager.LoadScene("GameScene");
     }
 
