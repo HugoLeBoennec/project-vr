8d1e3e8f220a1301fb5d05833787160cc1b2152c
EtienneRog <etienne.roger@viacesi.fr>
2022-01-18 11:40:57 +0100

    "Ajout des connexions inter-scene"

---
+++ Assets/VR-Vs-KMS/Scripts/MenuGestion.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuGestion : MonoBehaviour
+{
+
+    public void PlayGameKMS()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+    public void PlayGameVirus()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void QuitGame()
+    {
+        print("Unity, please Quit!!!");
+        Application.Quit();
+    }
+}
